8ec1fd1ae5b14a8b6ffe2d04957c3de1e38c0a3e
Lord-Bish <uchennabobbyfrade@yahoo.com>
2024-03-20 00:09:09 -0700

    corrections

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -21,7 +21,8 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
         "count": count,
         "limit": 100
     }
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     try:
         results = response.json()
         if response.status_code == 404:
